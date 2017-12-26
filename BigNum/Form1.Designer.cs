eac3d8f0966551a57e77910d02542b581cc1170a
炎凯 王 <ncabhd@hotmail.com>
2017-12-26 20:33:20 +0800

    添加项目文件。

---
+++ BigNum/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace BigNum
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.textBoxCalc = new System.Windows.Forms.TextBox();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.btnMul = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnInd = new System.Windows.Forms.Button();
+            this.btnEqu = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(117, 41);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(88, 39);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "打开文件";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(242, 41);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(88, 39);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "保存文件";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // textBoxCalc
+            // 
+            this.textBoxCalc.Location = new System.Drawing.Point(127, 157);
+            this.textBoxCalc.Name = "textBoxCalc";
+            this.textBoxCalc.Size = new System.Drawing.Size(244, 25);
+            this.textBoxCalc.TabIndex = 2;
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(127, 110);
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(244, 25);
+            this.textBoxResult.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "上次运算：";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(65, 219);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(55, 52);
+            this.btn1.TabIndex = 5;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(150, 219);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(55, 52);
+            this.btn2.TabIndex = 6;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(238, 219);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(55, 52);
+            this.btn3.TabIndex = 7;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(238, 294);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(55, 52);
+            this.btn6.TabIndex = 10;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(150, 294);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(55, 52);
+            this.btn5.TabIndex = 9;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(65, 294);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(55, 52);
+            this.btn4.TabIndex = 8;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(238, 367);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(55, 52);
+            this.btn9.TabIndex = 13;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(150, 367);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(55, 52);
+            this.btn8.TabIndex = 12;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(65, 367);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(55, 52);
+            this.btn7.TabIndex = 11;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(150, 438);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(55, 52);
+            this.btn0.TabIndex = 15;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Location = new System.Drawing.Point(316, 438);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(55, 52);
+            this.btnDiv.TabIndex = 19;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // btnMul
+            // 
+            this.btnMul.Location = new System.Drawing.Point(316, 367);
+            this.btnMul.Name = "btnMul";
+            this.btnMul.Size = new System.Drawing.Size(55, 52);
+            this.btnMul.TabIndex = 18;
+            this.btnMul.Text = "*";
+            this.btnMul.UseVisualStyleBackColor = true;
+            this.btnMul.Click += new System.EventHandler(this.btnMul_Click);
+            // 
+            // btnSub
+            // 
+            this.btnSub.Location = new System.Drawing.Point(316, 294);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(55, 52);
+            this.btnSub.TabIndex = 17;
+            this.btnSub.Text = "-";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.btnSub_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(316, 219);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(55, 52);
+            this.btnAdd.TabIndex = 16;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnInd
+            // 
+            this.btnInd.Location = new System.Drawing.Point(65, 438);
+            this.btnInd.Name = "btnInd";
+            this.btnInd.Size = new System.Drawing.Size(55, 52);
+            this.btnInd.TabIndex = 20;
+            this.btnInd.Text = "^";
+            this.btnInd.UseVisualStyleBackColor = true;
+            this.btnInd.Click += new System.EventHandler(this.btnInd_Click);
+            // 
+            // btnEqu
+            // 
+            this.btnEqu.Location = new System.Drawing.Point(238, 438);
+            this.btnEqu.Name = "btnEqu";
+            this.btnEqu.Size = new System.Drawing.Size(55, 52);
+            this.btnEqu.TabIndex = 21;
+            this.btnEqu.Text = "=";
+            this.btnEqu.UseVisualStyleBackColor = true;
+            this.btnEqu.Click += new System.EventHandler(this.btnEqu_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 541);
+            this.Controls.Add(this.btnEqu);
+            this.Controls.Add(this.btnInd);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnMul);
+            this.Controls.Add(this.btnSub);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.textBoxCalc);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.Text = "Calc";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox textBoxCalc;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Button btnMul;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnInd;
+        private System.Windows.Forms.Button btnEqu;
+    }
+}
+
---
+++ BigNum/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace BigNum
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("Math.dll", EntryPoint = "?Mul@MathDll@1@SAPBDPBD0HH@Z", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public extern static IntPtr Mul(String a, String b,int len_a,int len_b);
+
+        [DllImport("Math.dll", EntryPoint = "?Add@MathDll@1@SAPBDPBD0HH@Z", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public extern static IntPtr Add(String a, String b, int len_a, int len_b);
+
+        [DllImport("Math.dll", EntryPoint = "?Sub@MathDll@1@SAPBDPBD0HH@Z", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public extern static IntPtr Sub(String a, String b, int len_a, int len_b);
+
+        [DllImport("Math.dll", EntryPoint = "?Div@MathDll@1@SAPBDPBD0HH@Z", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public extern static IntPtr Div(String a, String b, int len_a, int len_b);
+
+        [DllImport("Math.dll", EntryPoint = "?Pow@MathDll@1@SAPBDPBD0HH@Z", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public extern static IntPtr Pow(String a, String b, int len_a, int len_b);
+
+
+        String s;
+        String num;
+        Boolean b = false;
+        int oper = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            s += "1";
+            textBoxCalc.Text = s;
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            s += "2";
+            textBoxCalc.Text = s;
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            s += "3";
+            textBoxCalc.Text = s;
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            s += "4";
+            textBoxCalc.Text = s;
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            s += "5";
+            textBoxCalc.Text = s;
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            s += "6";
+            textBoxCalc.Text = s;
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            s += "7";
+            textBoxCalc.Text = s;
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            s += "8";
+            textBoxCalc.Text = s;
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            s += "9";
+            textBoxCalc.Text = s;
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            s += "0";
+            textBoxCalc.Text = s;
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = true;
+            fileDialog.Title = "请选择文件";
+            fileDialog.Filter = "txt文件(*.txt)|*.txt";
+            fileDialog.ShowDialog();
+            String file = fileDialog.FileName;
+            StreamReader sr = new StreamReader(file);
+            String temp;
+            if((temp = sr.ReadLine()) != null)
+            {
+                num = temp;
+            }
+            sr.Close();
+            
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog fileDialog = new SaveFileDialog();
+            fileDialog.Title = "另存为";
+            fileDialog.Filter = "txt文件(*.txt)|*txt";
+            fileDialog.ShowDialog();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if(!b)
+            {
+                s += "+";
+                textBoxCalc.Text = s;
+                b = true;
+                oper = 1;
+            }
+        }
+
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+            if (!b)
+            {
+                s += "-";
+                textBoxCalc.Text = s;
+                b = true;
+                oper = 2;
+            }
+        }
+
+        private void btnMul_Click(object sender, EventArgs e)
+        {
+            if (!b)
+            {
+                s += "*";
+                textBoxCalc.Text = s;
+                b = true;
+                oper = 3;
+            }
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            if (!b)
+            {
+                s += "/";
+                textBoxCalc.Text = s;
+                b = true;
+                oper = 4;
+            }
+        }
+
+        private void btnInd_Click(object sender, EventArgs e)
+        {
+            if (!b)
+            {
+                s += "^";
+                textBoxCalc.Text = s;
+                b = true;
+                oper = 5;
+            }
+        }
+
+        private void btnEqu_Click(object sender, EventArgs e)
+        {
+            if (oper == 0)
+            {
+                MessageBox.Show("输入错误");
+            }
+            else {
+                String first = "", second = "";
+                String result = "";
+                int bit = 0;
+                for(int i = 0; i < s.Length; ++i)
+                {
+                    if(s[i]=='+'|| s[i] == '-'|| s[i] == '*'|| s[i] == '/'|| s[i] == '^')
+                    {
+                        bit = i;
+                        break;
+                    }
+                    first += s[i];
+                }
+                for(int i = bit + 1; i < s.Length; ++i)
+                {
+                    second += s[i];
+                }
+                switch (oper)
+                {
+                    case 1:
+                        IntPtr pAdd = Add(first, second, first.Length, second.Length);
+                        result = Marshal.PtrToStringAnsi(pAdd);
+                        s += "=" + result;
+                        textBoxCalc.Text = s;
+                        break;
+                    case 2:
+                        IntPtr pSub = Sub(first, second, first.Length, second.Length);
+                        result = Marshal.PtrToStringAnsi(pSub);
+                        s += "=" + result;
+                        textBoxCalc.Text = s;
+                        break;
+                    case 3:
+                        IntPtr pMul = Mul(first, second, first.Length, second.Length);
+                        result = Marshal.PtrToStringAnsi(pMul);
+                        s += "=" + result;
+                        textBoxCalc.Text = s;
+                        break;
+                    case 4:
+                        IntPtr pDiv = Div(first, second, first.Length, second.Length);
+                        result = Marshal.PtrToStringAnsi(pDiv);
+                        s += "=" + result;
+                        textBoxCalc.Text = s;
+                        break;
+                    case 5:
+                        IntPtr pPow = Pow(first, second, first.Length, second.Length);
+                        result = Marshal.PtrToStringAnsi(pPow);
+                        s += "=" + result;
+                        textBoxCalc.Text = s;
+                        break;
+                }
+            }
+        }
+    }
+}
